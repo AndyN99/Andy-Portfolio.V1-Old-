35d5e2d5bc7b2c360b097020979dbd09310dcbbb
Kagdya <103144613+Kagdya@users.noreply.github.com>
2022-04-10 20:57:13 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
-$('#sumbit').click(function() {
+const sendData = () => {
     $.ajax({
-       method: 'POST',
-       url: 'https://formsubmit.co/ajax/9andyN@gmail.com',
-       dataType: 'json',
-       accepts: 'application/json',
-       data: {
-           name: ".message_form",
-           message: "hello"
-       },
-       success: (data) => console.log(data),
-       error: (err) => console.log(err)
+        method: 'POST',
+        url: 'https://formsubmit.co/ajax/9andyN@gmail.com',
+        dataType: 'json',
+        accepts: 'application/json',
+        data: {
+            name: document.getElementById('name').value,
+            email:document.getElementById('email').value,
+            topic: document.getElementById('topic').value,
+            message: document.getElementById('message').value
+        },
+        success: (data) => console.log(data),
+        error: (err) => console.log(err)
     });
-   });
+}
+document.addEventListener('DOMContentLoaded', ()=> {
+    document.getElementById('sumbit').addEventListener('click',sendData());
+});
